769187190fb02b875ce9a49ef263350348e3f4c1
Marc Lopez <marc@marclp.es>
2023-03-11 16:30:35 +0300

    Master "working"

--- src/master.cpp
+++ src/master.cpp
@@ -1,61 +1,35 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <espnow.h>
-#include <Wire.h>
-//#include "SSD1306Ascii.h"
-//#include "SSD1306AsciiWire.h"
 #include "message.cpp"
 
-#define I2C_ADDRESS 0x3C
-uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t slave_address[] = {0xE8, 0xDB, 0x84, 0x99, 0xFE, 0x1D};
 
+struct_keepalive keepalive{};
+unsigned long last_time;
+unsigned long keepalive_interval = 500;
 
-struct_pairing pairing;
-struct_keepalive keepalive;
-
-bool paired = false;
-unsigned long last_time = 0;
-unsigned long keepalive_interval = 250;  // .5-sec between packets
-
-//SSD1306AsciiWire oled;
-
-void receive_pairing(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
-    memcpy(&pairing, incomingData, sizeof(pairing));
-    esp_now_add_peer(pairing.mac, ESP_NOW_ROLE_SLAVE, 1, nullptr, 0);
-    esp_now_unregister_recv_cb();
-    Serial.print("paired");
-}
-
-void send_keepalive() {
-    esp_now_send(nullptr, (uint8_t *) &pairing, sizeof(pairing));
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+    Serial.print("Last Packet Send Status: ");
+    if (sendStatus == 0){
+        Serial.println("Delivery success");
+    }
+    else{
+        Serial.println("Delivery fail");
+    }
 }
 
 void setup() {
-    Serial.begin(115200);
     WiFi.mode(WIFI_STA);
     esp_now_init();
-    esp_now_set_self_role(ESP_NOW_ROLE_MAX);
-    esp_now_register_recv_cb(receive_pairing);
-
-    //WiFi.macAddress(pairing_out.mac);
-    //pairing_out.channel = 0;
-
-    //keepalive.keepalive = 1;
-
-    //Wire.begin(2,0);
-    //Wire.setClock(400000L);
-    //oled.begin(&Adafruit128x64, I2C_ADDRESS);
-    //oled.setFont(System5x7);
-    //oled.clear();
-    //oled.print(" OK ");
-
+    esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+    esp_now_add_peer(slave_address, ESP_NOW_ROLE_COMBO, 1, nullptr, 0);
+    esp_now_register_send_cb(OnDataSent);
 }
 
 void loop() {
     unsigned long elapsed = millis() - last_time;
-    if (paired && elapsed > keepalive_interval) {
-        send_keepalive();
-        last_time = millis();
+    if (elapsed > keepalive_interval) {
+        esp_now_send(slave_address, (uint8_t *) &keepalive, sizeof(keepalive));
     }
-    yield();
 }
--- src/message.cpp
+++ src/message.cpp
@@ -1,13 +1,9 @@
 #include <cstdint>
 
-enum MessageType {PAIRING, KEEPALIVE};
+struct struct_pairing {
+    uint8_t mac[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+};
 
-
-typedef struct struct_pairing {
-    uint8_t mac[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};;
-} struct_pairing;
-
-typedef struct struct_keepalive {
-    uint8_t msgType = KEEPALIVE;
+struct struct_keepalive {
     uint8_t keepalive = 1;
-} struct_keepalive;
+};
