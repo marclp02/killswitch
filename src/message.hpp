dcf2fe18ac3a172aabf58621c6149c1a592edb16
Marc Lopez <marc@marclp.es>
2023-03-18 20:09:48 +0300

    message types

--- src/message.hpp
+++ src/message.hpp
@@ -4,8 +4,15 @@ enum MType {
     KEEP_ALIVE,
 };
 
+struct SlavePairingMessage {
+    MType mType = PAIRING_TO_MASTER;
+};
 
-struct Message {
-    MType type;
-    bool alive;
+struct MasterPairingMessage {
+    MType mType = PAIRING_TO_SLAVE;
+};
+
+struct KeepAliveMessage {
+    MType mType = KEEP_ALIVE;
+    int value = 1;
 };
