b3989bcda9f10c311d5587402c0cda16c6dc5467
jow1e <jow1e@cloud>
2023-04-15 18:03:49 +0300

    add include dir

---
+++ include/utils.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Arduino.h>
+
+
+uint8_t BROADCAST_ADDR[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+
+#define ADDRSIZE 6
+#define MESGSIZE 1
+
+
+enum Message: uint8_t {
+    BROADCAST,
+    KEEPALIVE_0,
+    KEEPALIVE_1,
+    BEEP
+};
